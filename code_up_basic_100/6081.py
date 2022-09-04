25c04a144411865922ef0c7e2ffe0bd796f3434f
Minju Kim <bluuminn@gmail.com>
2022-09-04 13:34:34 +0900

    Solve 6081 problem with another solution in code up basic 100

--- code_up_basic_100/6081.py
+++ code_up_basic_100/6081.py
@@ -1,3 +1,8 @@
-n = int(input(), 16)
+from sys import stdin, stdout
+
+_input = stdin.readline
+_print = stdout.write
+
+n = int(_input(), 16)
 for i in range(1, int('F', 16) + 1):
-    print('%X' % n, '*%X' % i, '=%X' % (n * i), sep='')
+    _print('%X' % n + '*%X' % i + '=%X' % (n * i) + '\n')
