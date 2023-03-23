c32a977b1c1de89614410ffd1ce9aa076223c5e9
River Kim <bluuminn@gmail.com>
2023-03-24 04:07:43 +0900

    [leetcode] design browser history 이중연결리스트 풀이

--- leetcode/design_browser_history.py
+++ leetcode/design_browser_history.py
@@ -1,107 +1,33 @@
 class Node:
-    def __init__(self, value, next_=None):
+    def __init__(self, value, prev=None, next_=None) -> None:
         self.value = value
+        self.prev = prev
         self.next_ = next_
-
-    def __str__(self) -> str:
-        return str(self.value)
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def __get_node(self, index: int):
-        if index < 0:
-            return None
-        node = self.head
-        for _ in range(index):
-            node = node.next_
-        return node
-
-    def get(self, index: int):
-        node = self.__get_node(index)
-        return node.value
-
-    def insert(self, index, value):
-        new_node = Node(value)
-        if self.__is_empty():
-            self.head = new_node
-            self.tail = new_node
-            return
-        prev_node = self.__get_node(index - 1)
-        if prev_node is None:
-            raise IndexError
-        if prev_node.next_ is None:
-            self.tail.next_ = new_node
-            self.tail = new_node
-            return
-        prev_node.next_ = new_node
-        self.tail = new_node
-
-    def append(self, value):
-        new_node = Node(value)
-        if self.__is_empty():
-            self.head = new_node
-            self.tail = new_node
-            return
-        self.tail.next_ = new_node
-        self.tail = new_node
-
-    def __is_empty(self) -> bool:
-        return self.head is None and self.tail is None
-
-    def __str__(self):
-        node = self.head
-        if node is None:
-            return "[]"
-        values = str(node)
-        while node.next_:
-            node = node.next_
-            values += ", " + str(node)
-        return "[" + values + "]"
 
 
 class BrowserHistory:
-
     def __init__(self, homepage: str):
-        self.histories = LinkedList()
-        self.histories.append(Node(homepage))
-        self.current = 0
-        self.size = 0
-        print(f"homepage: {homepage} / {self.current}")
+        self.current = Node(homepage)
 
     def visit(self, url: str) -> None:
-        self.current += 1
-        self.size += 1
-        if self.current == self.size:
-            self.histories.append(url)
-            print(f"visit1 {url} / {self.current} / {self.size}")
-            return
-        if self.size > self.current:
-            self.size = self.current
-        self.histories.insert(self.current, Node(url))
-        print(f"visit2 {url} / {self.current} / {self.size}")
+        new_node = Node(url)
+        self.current.next_ = new_node
+        new_node.prev = self.current
+        self.current = new_node
 
     def back(self, steps: int) -> str:
-        self.current -= steps
-        if self.current < 0:
-            self.current = 0
-        url = self.histories.get(self.current)
-        print(f"back {steps} / {url} / {self.current} / {self.size}")
-        return url
+        for _ in range(steps):
+            if self.current.prev is None:
+                break
+            self.current = self.current.prev
+        return self.current.value
 
     def forward(self, steps: int) -> str:
-        self.current += steps
-        if self.current > self.size:
-            self.current = self.size
-        url = self.histories.get(self.current)
-        print(f"forward {steps} / {url} / {self.current} / {self.size}")
-        return url
-
-    def __str__(self):
-        return self.histories.__str__()
+        for _ in range(steps):
+            if self.current.next_ is None:
+                break
+            self.current = self.current.next_
+        return self.current.value
 
 
 browser_history = BrowserHistory("esgriv.com")
