00df742ad5b9685490ccfbf2f938f7492d003186
River Kim <bluuminn@gmail.com>
2023-03-31 00:42:43 +0900

    [leetcode] valid parentheses 다시 풀이

--- leetcode/valid_parentheses.py
+++ leetcode/valid_parentheses.py
@@ -3,19 +3,17 @@ class Solution:
     def is_valid(s: str) -> bool:
         stack = list()
         for char in s:
-            if char in ['(', '{', '[']:
-                stack.append(char)
+            if char == '(':
+                stack.append(')')
                 continue
-            if char in [')', '}', ']']:
-                if not stack:
-                    return False
-                pop = stack.pop()
-                if char == ')' and pop != '(':
-                    return False
-                if char == '}' and pop != '{':
-                    return False
-                if char == ']' and pop != '[':
-                    return False
+            if char == '{':
+                stack.append('}')
+                continue
+            if char == '[':
+                stack.append(']')
+                continue
+            if not stack or stack.pop() != char:
+                return False
         return not stack
 
 
