744a4e5556904d1e6a52642e162e55a698d9dd3e
River Kim <bluuminn@gmail.com>
2023-04-01 01:29:41 +0900

    [leetcode] two sum 문제의 반환 타입대로 리턴하도록 변경

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -1,26 +1,27 @@
 from typing import List
 
 
-def two_sum(nums: List[int], target: int) -> bool:
-    nums_dict = dict()
-    for i, num in enumerate(nums):
-        nums_dict[num] = i
+class Solution:
+    @staticmethod
+    def two_sum(nums: List[int], target: int) -> List[int]:
+        nums_dict = dict()
+        for i, num in enumerate(nums):
+            nums_dict[num] = i
 
-    for i, num in enumerate(nums):
-        need = target - num
-        need_index = nums_dict.get(need, None)
-        i_index = nums_dict.get(num)
-        if num == need and nums.count(num) == 2:
-            return True
-        if need_index and need_index != i_index:
-            return True
-    return False
+        for i, num in enumerate(nums):
+            need = target - num
+            if num == need and nums.count(num) == 2:
+                return [i, nums.index(need, i + 1)]
+            need_index = nums_dict.get(need, None)
+            i_index = nums_dict.get(num)
+            if need_index and need_index != i_index:
+                return [i_index, need_index]
 
 
-print(two_sum([4, 1, 9, 7, 5, 3, 16], 14))
-print(two_sum([2, 1, 5, 7], 4))
-print(two_sum([2, 7, 11, 15], 9))
-print(two_sum([3, 2, 4], 6))
-print(two_sum([3, 3], 6))
-print(two_sum([3, 5, 2, 2, 3, 9], 10))
-print(two_sum([3, 5, 2, 2, 1, 9], 10))
+print(Solution.two_sum([4, 1, 9, 7, 5, 3, 16], 14))
+print(Solution.two_sum([2, 1, 5, 7], 4))
+print(Solution.two_sum([2, 7, 11, 15], 9))
+print(Solution.two_sum([3, 2, 4], 6))
+print(Solution.two_sum([3, 3], 6))
+print(Solution.two_sum([3, 5, 2, 3, 9], 10))
+print(Solution.two_sum([3, 5, 2, 2, 1, 9], 10))
