4501f85d72001f81d92d7c5401a3cfc55e2ef17e
River Kim <bluuminn@gmail.com>
2023-04-15 05:23:06 +0900

    [프로그래머스] 2018 kakao blind 1차 비밀지도

---
+++ programmers/kakao/2018_blind_1차_비밀지도.py
@@ -0,0 +1,13 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for i, j in zip(arr1, arr2):
+        number = bin(i | j)[2:]
+        number = number.zfill(n)
+        number = number.replace('1', '#')
+        number = number.replace('0', ' ')
+        answer.append(number)
+    return answer
+
+
+print(solution(n=5, arr1=[9, 20, 28, 18, 11], arr2=[30, 1, 21, 17, 28]))
+print(solution(n=6, arr1=[46, 33, 33, 22, 31, 50], arr2=[27, 56, 19, 14, 14, 10]))
