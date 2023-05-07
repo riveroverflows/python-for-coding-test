38c8297f1e4083b72df8764caffa3ae47eb99f57
River Kim <bluuminn@gmail.com>
2023-05-08 01:03:09 +0900

    [leetcode] lowest common ancestor of a binary tree

--- leetcode/lowest_common_ancestor_of_a_binary_tree.py
+++ leetcode/lowest_common_ancestor_of_a_binary_tree.py
@@ -4,11 +4,17 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def __str__(self):
+        return str(self.val)
+
+    def __eq__(self, other):
+        return self.val == other.val
+
 
 class Solution:
-    def lowest_common_ancestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowest_common_ancestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode'):
         if root is None:
-            return TreeNode(None)
+            return None
 
         left = self.lowest_common_ancestor(root.left, p, q)
         right = self.lowest_common_ancestor(root.right, p, q)
@@ -20,14 +26,19 @@ class Solution:
 
 
 root = TreeNode(3)
-root.left = TreeNode(5)
-root.right = TreeNode(1)
+node5 = TreeNode(5)
+root.left = node5
+node1 = TreeNode(1)
+root.right = node1
 root.left.left = TreeNode(6)
 root.left.right = TreeNode(2)
 root.right.left = TreeNode(0)
 root.right.right = TreeNode(8)
 root.left.right.left = TreeNode(7)
-root.left.right.right = TreeNode(4)
+node4 = TreeNode(4)
+root.left.right.right = node4
 
 s = Solution()
-print(s.lowest_common_ancestor(root=root, p=TreeNode(5), q=TreeNode(1)).val)
+print(s.lowest_common_ancestor(root=root, p=node5, q=node1))
+print(s.lowest_common_ancestor(root=root, p=TreeNode(5), q=TreeNode(4)))
+print(s.lowest_common_ancestor(root=root, p=node5, q=node4))
