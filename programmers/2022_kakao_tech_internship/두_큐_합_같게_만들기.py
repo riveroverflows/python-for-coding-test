1e8eb5afce6f108d1f42a54b801e07f6b2cc8114
River Kim <bluuminn@gmail.com>
2024-01-10 08:14:07 +0900

    [프로그래머스] 2022 kakao tech internship 두 큐 합 같게 만들기

--- programmers/2022_kakao_tech_internship/두_큐_합_같게_만들기.py
+++ programmers/2022_kakao_tech_internship/두_큐_합_같게_만들기.py
@@ -1,8 +1,26 @@
 def solution(queue1, queue2):
-    q1sum = sum(queue1)
-    q2sum = sum(queue2)
-    total = q1sum + q2sum
-    target = total // 2
+    n = len(queue1)
+    sum1, sum2 = sum(queue1), sum(queue2)
+    answer = 0
+    while answer < 4 * n + 1:
+        if sum1 == sum2:
+            return answer
+        if sum1 < sum2:
+            x = queue2[0]
+            del queue2[0]
+            sum1 += x
+            sum2 -= x
+            queue1.append(x)
+            answer += 1
+            continue
+        if sum1 > sum2:
+            x = queue1[0]
+            del queue1[0]
+            sum1 -= x
+            sum2 += x
+            queue2.append(x)
+            answer += 1
+    return -1
 
-    answer = -2
-    return answer
+
+print(solution([3, 2, 7, 2], [4, 6, 5, 1]))
