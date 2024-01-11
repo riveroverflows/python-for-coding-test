7aa6bbbc2d0c2789ac5c15c8de05f54e4d7de518
River Kim <bluuminn@gmail.com>
2024-01-11 21:42:25 +0900

    [프로그래머스] 2022 kakao tech internship 두 큐 합 같게 만들기 개선

--- programmers/2022_kakao_tech_internship/두_큐_합_같게_만들기.py
+++ programmers/2022_kakao_tech_internship/두_큐_합_같게_만들기.py
@@ -1,24 +1,26 @@
+from collections import deque
+
+
 def solution(queue1, queue2):
     n = len(queue1)
     sum1, sum2 = sum(queue1), sum(queue2)
+    deq1, deq2 = deque(queue1), deque(queue2)
     answer = 0
     while answer < 4 * n + 1:
         if sum1 == sum2:
             return answer
         if sum1 < sum2:
-            x = queue2[0]
-            del queue2[0]
+            x = deq2.popleft()
             sum1 += x
             sum2 -= x
-            queue1.append(x)
+            deq1.append(x)
             answer += 1
             continue
         if sum1 > sum2:
-            x = queue1[0]
-            del queue1[0]
+            x = deq1.popleft()
             sum1 -= x
             sum2 += x
-            queue2.append(x)
+            deq2.append(x)
             answer += 1
     return -1
 
