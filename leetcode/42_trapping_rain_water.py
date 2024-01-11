241f10a88886a20c78d846daa3a152e45129a6d0
KIM_MINJU29 <kim_minju29@eland.co.kr>
2024-01-12 08:24:09 +0900

    [leetcode] 42 trapping rain water 다시 풀어봄

--- leetcode/42_trapping_rain_water.py
+++ leetcode/42_trapping_rain_water.py
@@ -3,35 +3,36 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        copied_height = height[:]
+        trapped = height[:]
         stack = []
         top = [0, 0]
         for i, value in enumerate(height):
             while stack and stack[-1][1] < value:
                 top = stack.pop()
+
             if stack:
                 leftmost = stack[-1]
                 for j in range(leftmost[0] + 1, i):
-                    copied_height[j] = value
+                    trapped[j] = value
             else:
                 leftmost = top
-                for j in range(leftmost[0], i):
-                    copied_height[j] = leftmost[1]
+                for j in range(leftmost[0] + 1, i):
+                    trapped[j] = leftmost[1]
 
             stack.append([i, value])
 
         answer = 0
-        for i in range(len(height)):
-            answer += copied_height[i] - height[i]
+        for i, value in enumerate(trapped):
+            answer += value - height[i]
         return answer
 
 
 if __name__ == "__main__":
     solution = Solution()
-    # print(solution.trap([1, 0, 2, 1, 0, 1, 3, 1, 1, 2]))
-    # print(solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
-    # print(solution.trap([4, 2, 0, 3, 2, 5]))
-    # print(solution.trap([4, 2, 3]))
+    print(solution.trap([1, 0, 2, 1, 0, 1, 3, 1, 1, 2]))
+    print(solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+    print(solution.trap([4, 2, 0, 3, 2, 5]))
+    print(solution.trap([4, 2, 3]))
     print(solution.trap([0, 7, 1, 4, 6]))
-    # print(solution.trap([3, 9, 2, 2, 8, 8, 7, 3]))
+    print(solution.trap([3, 9, 2, 2, 8, 8, 7, 3]))
     print(solution.trap([4, 2, 0, 3, 2, 4, 3, 4]))
