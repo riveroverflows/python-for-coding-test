8350486a41f100ad598a02e3fb797927c0fc1fb5
River Kim <bluuminn@gmail.com>
2024-01-11 21:41:52 +0900

    [leetcode] 739. daily temperatures black 적용

--- leetcode/739_daily_temperatures.py
+++ leetcode/739_daily_temperatures.py
@@ -13,7 +13,7 @@ class Solution:
         return answer
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     s = Solution()
     print(s.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
     print(s.dailyTemperatures([30, 40, 50, 60]))
