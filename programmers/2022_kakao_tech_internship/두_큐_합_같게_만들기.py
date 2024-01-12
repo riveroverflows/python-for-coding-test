fc1f1a1a3fadbb8777818bf30bc8bae5f7d8f356
River Kim <bluuminn@gmail.com>
2024-01-12 19:53:12 +0900

    [프로그래머스] 2022 kakao tech internship 두 큐 합 같게 만들기 투포인터 풀이

--- programmers/2022_kakao_tech_internship/두_큐_합_같게_만들기.py
+++ programmers/2022_kakao_tech_internship/두_큐_합_같게_만들기.py
@@ -1,28 +1,30 @@
-from collections import deque
-
-
 def solution(queue1, queue2):
+    q = queue1 + queue2
     n = len(queue1)
+    left, mid = 0, n
     sum1, sum2 = sum(queue1), sum(queue2)
-    deq1, deq2 = deque(queue1), deque(queue2)
-    answer = 0
-    while answer < 4 * n + 1:
+
+    for i in range(n * 4 + 1):
         if sum1 == sum2:
-            return answer
+            return i
         if sum1 < sum2:
-            x = deq2.popleft()
-            sum1 += x
-            sum2 -= x
-            deq1.append(x)
-            answer += 1
+            num = q[mid]
+            sum1 += num
+            sum2 -= num
+            mid += 1
+            if mid >= n * 2:
+                mid = 0
             continue
         if sum1 > sum2:
-            x = deq1.popleft()
-            sum1 -= x
-            sum2 += x
-            deq2.append(x)
-            answer += 1
+            num = q[left]
+            sum1 -= num
+            sum2 += num
+            left += 1
+            if left >= n * 2:
+                left = 0
     return -1
 
 
-print(solution([3, 2, 7, 2], [4, 6, 5, 1]))
+print(solution([3, 2, 7, 2], [4, 6, 5, 1]))  # 2
+print(solution([1, 2, 1, 2], [1, 10, 1, 2]))  # 7
+print(solution([1, 1], [1, 5]))  # -1
