d05fe58d3236d61b9f7c42fa1d5e3784343af9f9
River Kim <bluuminn@gmail.com>
2024-01-12 20:28:38 +0900

    [프로그래머스] 2022 kakao tech internship 두 큐 합 같게 만들기 투포인터 리스트 안 합친 풀이

--- programmers/2022_kakao_tech_internship/두_큐_합_같게_만들기.py
+++ programmers/2022_kakao_tech_internship/두_큐_합_같게_만들기.py
@@ -1,27 +1,28 @@
 def solution(queue1, queue2):
-    q = queue1 + queue2
     n = len(queue1)
-    left, mid = 0, n
+    left, mid = 0, 0
     sum1, sum2 = sum(queue1), sum(queue2)
-
+    q1lo, q2lo = 'l', 'r'
     for i in range(n * 4 + 1):
         if sum1 == sum2:
             return i
         if sum1 < sum2:
-            num = q[mid]
+            num = queue2[mid] if q2lo == 'r' else queue1[mid]
             sum1 += num
             sum2 -= num
             mid += 1
-            if mid >= n * 2:
+            if mid >= n:
                 mid = 0
+                q2lo = 'l' if q2lo == 'r' else 'r'
             continue
         if sum1 > sum2:
-            num = q[left]
+            num = queue1[left] if q1lo == 'l' else queue2[left]
             sum1 -= num
             sum2 += num
             left += 1
-            if left >= n * 2:
+            if left >= n:
                 left = 0
+                q1lo = 'r' if q1lo == 'l' else 'l'
     return -1
 
 
