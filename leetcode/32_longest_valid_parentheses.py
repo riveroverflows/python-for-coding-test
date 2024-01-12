e1a73120c0f8281ccac51819c90d80cd58b187f3
River Kim <bluuminn@gmail.com>
2024-01-13 00:01:36 +0900

    [leetcode] 32. longest valid parentheses

--- leetcode/32_longest_valid_parentheses.py
+++ leetcode/32_longest_valid_parentheses.py
@@ -1,25 +1,35 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+        stack = []
         answer = 0
-        if len(s) < 2:
-            return 0
         for i, ch in enumerate(s):
             if ch == "(":
-                if i == 0:
-                    answer += 1
+                stack.append([i, ch])
+                continue
+            if ch == ")":
+                if not stack:
+                    stack.append([i, ch])
                     continue
-                if i > 0:
-                    if s[i - 1] == ")" and i + 1 < len(s) and s[i + 1] == ")":
-                        answer += 1
+                if stack[-1][1] == "(":
+                    stack.pop()
+                else:
+                    stack.append([i, ch])
+                    continue
+            if stack:
+                length = i - stack[-1][0]
             else:
-                if i > 0:
-                    if s[i - 1] == "(":
-                        answer += 1
+                length = i + 1
 
+            answer = max(answer, length)
         return answer
 
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.longestValidParentheses(")("))
-    print(solution.longestValidParentheses("(()"))
+    print(solution.longestValidParentheses(")(()))()()()"))  # 6
+    print(solution.longestValidParentheses("()"))  # 2
+    print(solution.longestValidParentheses(")("))  # 0
+    print(solution.longestValidParentheses("(()"))  # 2
+    print(solution.longestValidParentheses(")()())"))  # 4
+    print(solution.longestValidParentheses(""))  # 0
+    print(solution.longestValidParentheses("()(()"))  # 2
