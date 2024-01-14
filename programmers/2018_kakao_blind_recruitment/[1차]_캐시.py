29c2497e29c5093622015a51de112160e1a9fb32
River Kim <bluuminn@gmail.com>
2024-01-14 17:59:34 +0900

    [프로그래머스] 2018 kakao blind recruitment [1차] 캐시 리스트 풀이

--- programmers/2018_kakao_blind_recruitment/[1차]_캐시.py
+++ programmers/2018_kakao_blind_recruitment/[1차]_캐시.py
@@ -1,55 +1,36 @@
-from math import inf
+from collections import deque
 
 
 def solution(cacheSize, cities):
     answer = 0
-    cache = {}
+    cache = deque()
     if cacheSize == 0:
         return len(cities) * 5
 
-    for i, city in enumerate(cities):
-        city_lower = city.lower()
-        if len(cache) + 1 <= cacheSize:
-            cache[city_lower] = (i, 1)
-            answer += 5
-        else:
-            if city_lower in cache:
-                index, hits = cache.get(city_lower)
-                hits += 1
-                cache[city_lower] = (index, hits)
-                answer += 1
-            else:
-                # hits 수가 제일 작은 것 중에 제일 빨리 추가된 거
-                min_hits = inf
-                min_idx = inf
-                min_city = ''
-                for k, v in cache.items():
-                    idx, hits = v
-                    if min_hits > hits:
-                        min_hits = hits
-                        min_idx = idx
-                        min_city = k
-                        continue
-                    if min_hits == hits:
-                        if min_idx > idx:
-                            min_idx = idx
-                            min_city = k
+    for city in cities:
+        lcity = city.lower()
+        if lcity in cache:
+            cache.remove(lcity)
+            cache.append(lcity)
+            answer += 1
+            continue
 
-                del cache[min_city]
-                cache[city_lower] = (i, 1)
-                answer += 5
+        if len(cache) >= cacheSize:
+            cache.popleft()
+
+        cache.append(lcity)
+        answer += 5
 
     return answer
 
-"""
+
 print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))  # 50
 print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))  # 21
 print(solution(2,
                ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork",
                 "Rome"]))  # 60
-"""
 print(solution(5,
                ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork",
                 "Rome"]))  # 52
-# print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))  # 16
-# print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))  # 25
+print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))  # 16
+print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))  # 25
