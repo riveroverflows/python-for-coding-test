945f423c3c62100efb59afafc74b31a279159fa3
River Kim <bluuminn@gmail.com>
2024-01-15 22:59:51 +0900

    [프로그래머스] 2022 kakao tech internship 행렬과 연산 dfs 시간 초과

--- programmers/2022_kakao_tech_internship/행렬과_연산.py
+++ programmers/2022_kakao_tech_internship/행렬과_연산.py
@@ -1,46 +1,39 @@
 import copy
-from collections import deque
 
 
 def solution(rc, operations):
     def rotate(matrix):
         copied = copy.deepcopy(matrix)
-        row = len(matrix)
-        col = len(matrix[0])
-        visited = [[False] * col for _ in range(row)]
-        for i in range(1, row - 1):
-            for j in range(1, col - 1):
-                visited[i][j] = True
+        row_len = len(matrix)
+        col_len = len(matrix[0])
+        news = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        start_r, start_c = 0, 0
+        start_v = matrix[start_r][start_c]
 
-        def bfs(x, y):
-            q = deque()
-            q.append((x, y))
-            visited[x][y] = True
+        for i in {0, row_len - 1}:
+            for j in range(col_len):
+                copied[i][j] = -1
+        for i in {0, col_len - 1}:
+            for j in range(row_len):
+                copied[j][i] = -1
 
-            while q:
-                cur_x, cur_y = q.popleft()
-                if cur_x == 0:
-                    for next_y in range(cur_y + 1, col):
-                        copied[cur_x][next_y] = matrix[cur_x][next_y - 1]
-                elif cur_x == row - 1:
-                    for next_y in range(col - 1, 0, -1):
-                        copied[cur_x][next_y - 1] = matrix[cur_x][next_y]
-                else:
-                    if cur_y == 0:
-                        for next_x in range(row - 1, 0, -1):
-                            copied[next_x - 1][cur_y] = matrix[next_x][cur_y]
-                    if cur_y == col - 1:
-                        for next_x in range(cur_x, row):
-                            copied[next_x][cur_y] = matrix[next_x - 1][cur_y]
+        def is_valid(r, c):
+            return 0 <= r < row_len and 0 <= c < col_len and copied[r][c] == -1
 
-        for i in range(row):
-            for j in range(col):
-                if not visited[i][j]:
-                    bfs(i, j)
+        def dfs(r, c, v):
+            for dr, dc in news:
+                next_r = r + dr
+                next_c = c + dc
+                if is_valid(next_r, next_c):
+                    if copied[next_r][next_c] == -1:
+                        copied[next_r][next_c] = v
+                        next_v = matrix[next_r][next_c]
+                        dfs(next_r, next_c, next_v)
 
+        dfs(start_r, start_c, start_v)
         return copied
 
-    def shiftrow(matrix):
+    def shift_row(matrix):
         copied = [0] * len(matrix)
         for i, row in enumerate(matrix):
             if i + 1 >= len(matrix):
@@ -54,9 +47,10 @@ def solution(rc, operations):
         if "Rotate" == op:
             answer = rotate(answer)
         if "ShiftRow" == op:
-            answer = shiftrow(answer)
+            answer = shift_row(answer)
     return answer
 
 
+print(solution([[1, 2, 3], [4, 5, 6], [7, 8, 9]], ["Rotate"]))
 print(solution([[1, 2, 3], [4, 5, 6], [7, 8, 9]], ["Rotate", "ShiftRow"]))
 print(solution([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], ["ShiftRow", "Rotate", "ShiftRow", "Rotate"]))
