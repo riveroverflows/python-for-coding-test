d3374e90390caeee24acd76e728a4d982f95a1d3
River Kim <bluuminn@gmail.com>
2024-02-21 22:33:54 +0900

    6주차/house robber top-down 풀이

--- week6/asm1_house_robber.py
+++ week6/asm1_house_robber.py
@@ -4,16 +4,19 @@ from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [-1] * n
-        dp[0] = nums[0]
+        memo = dict()
+        memo[0] = nums[0]
 
-        if n > 1:
-            dp[1] = max(nums[0], nums[1])
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
+            if i not in memo:
+                memo[i] = max(dp(i - 2) + nums[i], dp(i - 1))
+            return memo[i]
 
-            for i in range(2, n):
-                dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
-
-        return dp[n - 1]
+        return dp(n - 1)
 
 
 if __name__ == "__main__":
