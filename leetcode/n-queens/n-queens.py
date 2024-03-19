53ab01d89f51ed8a34ce4133561109d2910dd626
River Kim <bluuminn@gmail.com>
2024-03-19 21:30:17 +0900

    [leetcode] N-Queens
    - 백트래킹 풀이: 해설 코드 참조

--- leetcode/n-queens/n-queens.py
+++ leetcode/n-queens/n-queens.py
@@ -1,49 +1,43 @@
-import itertools
 from typing import List
 
 
 class Solution:
-    def met_the_queen(self, n, r, c, dr, dc, chessboard):
-        # 다음 위치가 Q가 아니면 ok
-        while 0 <= r < n and 0 <= c < n:
-            if chessboard[r][c] == "Q":
-                return True
-            r += dr
-            c += dc
-        return False
-
     def solveNQueens(self, n: int) -> List[List[str]]:
-        chess_rc = []
-        for r in range(n):
-            for c in range(n):
-                chess_rc.append((r, c))
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        def is_valid(row, col):
+            # 현재 열에 퀸이 있는지 체크
+            # 0부터 현재 위치 전까지
+            # ex backtracking(3) -> range: 0 ~ 2
+            for r in range(row):
+                if chessboard[r][col] == "Q":
+                    return False
+
+            # 양 대각선 체크
+            for dr, dc in [(-1, -1), (-1, 1)]:
+                nr, nc = row + dr, col + dc
+                while 0 <= nr < n and 0 <= nc < n:
+                    if chessboard[nr][nc] == "Q":
+                        return False
+                    nr += dr
+                    nc += dc
+
+            # 여기까지 왔으면 valid 함
+            return True
+
+        def backtracking(row):
+            if row == n:
+                answer.append(["".join(r) for r in chessboard])
+
+            # row의 열 순회
+            for col in range(n):
+                # chessboard[row][col] 에 Q를 위치 시켜도 되는지 확인
+                if is_valid(row, col):
+                    chessboard[row][col] = "Q"
+                    backtracking(row + 1)
+                    chessboard[row][col] = "."
+
         answer = []
-        for queens in itertools.combinations(chess_rc, n):
-            # 보드 초기화
-            chessboard = [["."] * n for _ in range(n)]
-
-            # 퀸 좌표 입력
-            for queen in queens:
-                qr, qc = queen
-                chessboard[qr][qc] = "Q"
-
-            met_the_queen = False
-            for queen in queens:
-                qr, qc = queen
-                for dr, dc in directions:
-                    nr, nc = qr + dr, qc + dc
-                    # 보드판 범위 확인
-                    # Q 만났으면 break
-                    if self.met_the_queen(n, nr, nc, dr, dc, chessboard):
-                        met_the_queen = True
-                        break
-            if not met_the_queen:
-                board = []
-                for row in chessboard:
-                    board.append("".join(row))
-                answer.append(board)
-
+        chessboard = [["."] * n for _ in range(n)]
+        backtracking(0)
         return answer
 
 
